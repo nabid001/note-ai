5ea6095eef18e618751f9016fdabf9e0fc6307fd
Nabid <nabid.explore@gmail.com>
2024-06-23 00:24:51 +0600

    modify middleware

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import Voice from "@/components/Voice";
+import { UserProfile } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center flex-col gap-3">
       {/* <h1>Hello World</h1> */}
+
+      {/* <UserProfile /> */}
 
       <Voice />
     </div>
